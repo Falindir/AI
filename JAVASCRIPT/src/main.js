bc29641da3ad623102299f0c136b646a38663429
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2016-01-26 13:56:51 +0100

    Add ai with JAVASCRIPT

---
+++ JAVASCRIPT/src/main.js
@@ -0,0 +1,7 @@
+if (typeof console === 'object') {
+    console.log('Hello Master Falindir');
+} else if (typeof document === 'object') {
+    document.write('Hello Master Falindir');
+} else {
+    print('Hello Master Falindir');
+}
