4dc95cbd674fd60f81fcc56590e746295804b0db
jlopez <jimmy.lopez0023@gmail.com>
2017-01-27 14:01:27 +0100

    Add ai with Assembly

---
+++ Assembly/src/ai.s
@@ -0,0 +1,35 @@
+                    .data 
+
+msg :               .asciz "Hello Master Falindir\n" 
+
+len = . - msg 
+
+                    .bss 
+
+                    .text 
+
+                    .global _start 
+
+     
+
+_start : 
+
+                     movl $msg,%ecx 
+
+                     movl $len,%edx 
+
+                     movl $1,%ebx 
+
+                     movl $4,%eax 
+
+                     int  $0x80           # appel système 
+
+     
+
+exit : 
+
+                     movl $0,%ebx 
+
+                     movl $1,%eax 
+
+                     int  $0x80 
